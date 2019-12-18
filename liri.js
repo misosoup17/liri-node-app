9b2f6b268aca8345f051c494f91481b04de98898
misosoup17 <misagaspar@icloud.com>
2019-12-18 15:34:20 -0800

    updated

--- liri.js
+++ liri.js
@@ -1,137 +1,161 @@
 require("dotenv").config();
-var twitter = require("twitter");
-var Spotify = require("node-spotify-api");
-const keys = require("./keys.js");
-var inquirer = require("inquirer");
-//Twitter API Keys
-var client = new twitter(keys.twitter);
-// Spotify API Keys
-var spotify = new Spotify(keys.spotify);
 
-//------------------------
-
-var nodeArgs = process.argv;
-var params = {
-    screen_name: 'T1BasicMountain'
-}
-//This function will pull the most recent tweets for my twitter profile (this profile has only 20 tweets, all forming the poem Invictus by William E. Henley, at the time of writing this at least)
-function recallTweets() {
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            tweets.reverse().forEach(element => {
-                console.log(`
-            ------
-            ${element.text}`
-                    //${element.created_at} was omitted because my 20 tweets are park of a poem, and that makes the formatting look ugly.
-                );
-            });
-            liriInit();
-
-        }
-    })
-};
-//if songTitle is blank, replace it with 'The Sign'
-function playSong() {
-    inquirer.prompt([{
-        type: "input",
-        message: "What do you want me to look up?",
-        name: "query"
-    }]).then(function (inquirerResponse) {
-        spotify.search({
-            type: 'track',
-            query: inquirerResponse.query,
-            limit: 1
-        }, function (err, data) {
-            if (!err) {
-                var songObj = data.tracks.items[0];
-                var artistName = songObj.album.artists[0].name;
-                console.log(`
-            Artist: ${artistName}
-            Song title: ${inquirerResponse.query}
-            Album Name: ${songObj.album.name}
-            Preview URL: ${songObj.preview_url}`);
-                liriInit();
-            } else {
-                console.log(`Error: ${err}`);
-            }
-        })
-    })
-};
-// playSong(songTitle);
-
-//OMDB API call
-
+//VARS
 var request = require("request");
-//Parse movieName to add +'s
-function movieQuery() {
-    inquirer.prompt([{
-        type: "input",
-        message: "What do you want me to look up?",
-        name: "query"
-    }]).then(function (inquirerResponse) {
-        request(`http://www.omdbapi.com/?t=${inquirerResponse.query}&apikey=trilogy`, function (err, response, body) {
-            if (!err && response.statusCode === 200) {
-                var movieObj = JSON.parse(body);
-                // console.log(movieObj);
-                console.log(`
-                Movie Title: ${movieObj.Title}
-                Year Released: ${movieObj.Year}
-                IMDB Rating: ${movieObj.Ratings[0].Value}
-                Rotten Tomtoes Rating: ${movieObj.Ratings[1].Value}
-                Produced in: ${movieObj.Country}
-                Language(s): ${movieObj.Language}
-                Plot: ${movieObj.Plot}
-                Actors: ${movieObj.Actors}
-                 `);
-                 liriInit();
-            }
-        })
-    })
-};
-//Opens the text file and reads what's inside, triggering a command
 var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//vars to capture user inputs.
+var userOption = process.argv[2]; 
+var inputParameter = process.argv[3];
 
-function doThis() {
-    fs.readFile('random.txt', 'utf8', function (error, data) {
-        if (error) {
-            return console.log(error);
+//Execute function
+UserInputs(userOption, inputParameter);
+
+//FUNCTIONS
+function UserInputs (userOption, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
+        break;
+    case 'spotify-this-song':
+        showSongInfo(inputParameter);
+        break;
+    case 'movie-this':
+        showMovieInfo(inputParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+    default: 
+        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
+}
+
+//Funtion for Concert Info: Bands in Town
+function showConcertInfo(inputParameter){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
+    request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var concerts = JSON.parse(body);
+        for (var i = 0; i < concerts.length; i++) {  
+            console.log("**********EVENT INFO*********");  
+            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
+            console.log(i);
+            fs.appendFileSync("log.txt", i+"\n");
+            console.log("Name of the Venue: " + concerts[i].venue.name);
+            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+            console.log("Venue Location: " +  concerts[i].venue.city);
+            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+            console.log("Date of the Event: " +  concerts[i].datetime);
+            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+            console.log("*****************************");
+            fs.appendFileSync("log.txt", "*****************************"+"\n");
         }
-        console.log(data);
-    })
+    } else{
+      console.log('Error occurred.');
+    }
+});}
+
+//Funtion for Music Info: Spotify
+function showSongInfo(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "The Sign"; //default Song
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("**********SONG INFO*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i +"\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");  
+                fs.appendFileSync("log.txt", "*****************************\n");
+             }
+        }
+    );
 };
 
+//Funtion for Movie Info: OMDB
+function showMovieInfo(inputParameter){
+    if (inputParameter === undefined) {
+        inputParameter = "Mr. Nobody"
+        console.log("-----------------------");
+        fs.appendFileSync("log.txt", "-----------------------\n");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +"\n");
+        console.log("It's on Netflix!");
+        fs.appendFileSync("log.txt", "It's on Netflix!\n");
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=b3c0b435";
+    request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var movies = JSON.parse(body);
+        console.log("**********MOVIE INFO*********");  
+        fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
+        console.log("Title: " + movies.Title);
+        fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
+        console.log("Release Year: " + movies.Year);
+        fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
+        console.log("IMDB Rating: " + movies.imdbRating);
+        fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
+        console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies));
+        fs.appendFileSync("log.txt", "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies) + "\n");
+        console.log("Country of Production: " + movies.Country);
+        fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
+        console.log("Language: " + movies.Language);
+        fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
+        console.log("Plot: " + movies.Plot);
+        fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
+        console.log("Actors: " + movies.Actors);
+        fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
+        console.log("*****************************");  
+        fs.appendFileSync("log.txt", "*****************************\n");
+    } else{
+      console.log('Error occurred.');
+    }
 
+});}
 
-function liriInit() {
-    inquirer.prompt([{
-            type: "list",
-            message: "Hello! What would you like to do?",
-            choices: ["Read my tweets", "Spotify this song", "Movie this!", "Do whatever"],
-            name: "userCommand"
-        },
-        {
-            type: "confirm",
-            message: "Are you sure?",
-            name: "confirm",
-            default: true
-        }
-    ]).then(function (inquirerResponse) {
-        if (inquirerResponse.confirm) {
-            switch (inquirerResponse.userCommand) {
-                case "Read my tweets":
-                    recallTweets();
-                    break;
-                case "Spotify this song":
-                    playSong();
-                    break;
-                case "Movie this!":
-                    movieQuery();
-                    break;
-                case "Do whatever":
-                    doThis()
-                    break;
-            }
-        }
-    })
+//function to get proper Rotten Tomatoes Rating
+function getRottenTomatoesRatingObject (data) {
+    return data.Ratings.find(function (item) {
+       return item.Source === "Rotten Tomatoes";
+    });
+  }
+  
+  function getRottenTomatoesRatingValue (data) {
+    return getRottenTomatoesRatingObject(data).Value;
+  }
+
+//function for reading out of random.txt file  
+function showSomeInfo(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+		if (err){ 
+			return console.log(err);
+		}
+        var dataArr = data.split(',');
+        UserInputs(dataArr[0], dataArr[1]);
+	});
 }
-liriInit();
